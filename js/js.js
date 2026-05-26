daacf6dd713a448bc9781d14ca75092d035c075f
eaa25jkni <eaa25jkni@students.eaaa.dk>
2026-05-26 09:34:27 +0200

    Array med spørgsmål, som indeholder antributterne tekst, svarmuligheder og svar
    Her er der lavet et array med spørgsmålene, som vi senere skal hente

---
+++ js/js.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//Array med spørgsmål
+
+const spoergsmål = [
+  {
+    tekst: "Hvad lægger du først mærke til, når du ser dig selv i et spejl?",
+    svarmuligheder: [
+      "Ensom",
+      "Alle mine fejl",
+      "Mit smil",
+      "Mine øjne",
+      "En taber",
+      "Min hårfarve",
+      "Skriv dit eget svar",
+    ],
+  },
+  {
+    tekst: "Hvis andre skulle beskrive dig med ét ord, hvad ville de sige?",
+    svarmuligheder: ["Dum", "Ond", "Grim", "Sød", "Skør"],
+  },
+  {
+    tekst: "Hvad er din største usikkerhed?",
+    svarmuligheder: [
+      "Mit grin",
+      "Mine hænder",
+      "At blive misforstået",
+      "Ikke klog nok",
+      "Ikke god nok",
+    ],
+    svar: "svaret"
+  },
+];
+
+console.log(spoergsmål);
